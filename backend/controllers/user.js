4a1b279c614050c71d8030989b07f9b98a8bc884
SonikaSahana <deoghariasonika@gmail.com>
2024-02-19 18:25:19 +0530

    backend for signup

---
+++ backend/controllers/user.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path=require("path");
+
+exports.signupPage = (req,res,next) => {
+    res.sendFile(path.join(__dirname,'..','..','frontend','signup.html'));
+}
+const User = require('../models/user')
+
+exports.createUser = async (req,res)=>{
+    try{
+
+    
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    let result = await User.findOne({where : {email : email}})
+    console.log(result)
+    if(result !== null)
+        return res.status(401).json({success : false , msg : "User already exists"})
+    let hash = await bcrypt.hash(password , 10);
+    const user = await User.create({name : name , email : email , password : hash})
+    
+    const userWithoutPassword = user.toJSON();
+    console.log(userWithoutPassword)
+    delete userWithoutPassword.password;
+    
+    return res.json(userWithoutPassword);
+    }catch(e){
+        return res.status(500).json({msg : "Internal server error"})
+    }
+}
+
