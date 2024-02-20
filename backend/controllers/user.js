7f03eb64968a555305bdb4bc6c34339f241ff2e1
SonikaSahana <deoghariasonika@gmail.com>
2024-02-20 14:13:44 +0530

    backend for login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path=require("path");
+const bcrypt = require('bcrypt');
+//const jwt = require('jsonwebtoken')
 
 exports.signupPage = (req,res,next) => {
-    res.sendFile(path.join(__dirname,'..','..','frontend','signup.html'));
+    res.sendFile(path.join(__dirname,'..','..','frontend'));
 }
 const User = require('../models/user')
 
@@ -29,4 +31,23 @@ exports.createUser = async (req,res)=>{
         return res.status(500).json({msg : "Internal server error"})
     }
 }
+
+exports.login = async (req ,res)=>{
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+        const user = await User.findOne({where : {email : email}})
+        
+ 
+        if(user === null){
+
+            return res.status(401).json({success : false , msg : "wrong credentials"})
+        }
+    }
+        catch(err){
+          return err
+        }
+
+    }
+
 
