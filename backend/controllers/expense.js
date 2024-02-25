20b42bb2b68fe221b15a7cd4e056b6e408958695
SonikaSahana <deoghariasonika@gmail.com>
2024-02-25 11:29:20 +0530

    fixed some bugs

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -1,60 +1,73 @@
-const User = require('../models/user');
-const path = require('path');
-const express = require('express');
-const { JSON } = require('sequelize');
+const Expense = require('../models/expense')
+const sequelize = require('../util/db')
 
-
-
-
-exports.getExpensesPage = (req,res,next) => {
-    res.sendFile(path.join(__dirname,'..','public', 'views','expenseTracker.html'));
-     
-}
-
-exports.getExpenses = (req, res, next) => {
-   
-   
-
-
-   
-}
-
-
-exports.postRegister = (req,res,next) => {
-    console.log('PRINT kkkkkk..')
-    console.log(req.body.description)
-    console.log(req.body.amount)
-    let expenses = {
-        amount: req.body.amount,
-        description: req.body.description,
-        catagory:req.body.catagory
-    }
-
-    User.create({
-        amount: expenses.amount,
-        description:expenses.description,
-        catagory:expenses.catagory
-
+exports.getAll = (req, res) => {
+    req.user.getExpenses({
+        raw: true,
+        attributes: ["id", "expense", "category", "description"]
     })
-    .then( result => {
-        console.log('Created..')
-       res.redirect('/successfullyadded');
+        .then(data => {
+            return res.json({ data })
+        })
+        .catch(e => {
+            console.log(e)
+            return res.status(500).json({ msg: "Internal server error" })
+        })
+}
+
+exports.addExpense = async (req, res) => {
+    const expense = req.body.expense;
+    const description = req.body.description;
+    const category = req.body.category;
+    const t = await sequelize.transaction()
+
+    req.user.createExpense({
+        expense: expense,
+        description: description,
+        category: category
+    }, {
+        transaction: t
+    }).then(async (data) => {
+        let newAmount = req.user.totalAmount + +expense;
+        await req.user.update({ totalAmount: newAmount }, {
+            transaction: t
+        })
+        await t.commit()
+        return res.json({ data })
     })
-    .catch( err => {
-        res.send("invalid")
-       // res.sendStatus(500).json(err)
-        console.log(err)
-    });
-
-   
+        .catch(async e => {
+            console.log(e)
+            await t.rollback()
+            return res.status(403).json({ success: false })
+        })
 }
 
-exports.getBookingSuccess = ( req, res, next) => {
-  res.send(' Successfully Registered')
-  
+exports.deleteExpense = (req, res) => {
+    const id = req.params.id;
+    req.user.getExpenses({ where: { id: id } }).then(async expense => {
+        req.user.totalAmount = Number(req.user.totalAmount) - Number(expense[0].expense)
+        console.log(expense)
+        await req.user.save()
+        return expense[0].destroy()
+    }).then(() => {
+        return res.status(200).json({ success: true, msg: "deleted successfully" })
+    }).catch(e => {
+        console.log(e)
+        return res.status(401).json({ success: false })
+    })
 }
 
-exports.postDeleteUer = () => {
-
-};
-
+exports.editExpense = (req, res) => {
+    const id = req.params.id;
+    req.user.getExpenses({ where: { id: id } }).then(data => {
+        data[0].expense = req.body.expense,
+            data[0].description = req.body.description,
+            data[0].category = req.body.category
+        return data[0].save()
+    }).then(() => {
+        return res.json({ success: true })
+    }).catch(e => {
+        console.log(e)
+        return res.status(403).json({ success: false })
+    })
+}
--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -2,25 +2,21 @@ const express = require('express')
 const router = express.Router();
 
 
-//const Expense = require('../models/expense')
-const expense = require('../controllers/expense')
+const Expense = require('../models/expense')
+const expense = require('../controller/expense')
 
-//const authenticate = require('../middleware/auth')
-
-
-//router.get('/' ,authenticate, expense.getAll) //fetch all the expense 
-
-router.post('/add-expense' , expense.addExpense) // add a new expense
-
-router.delete('/deleteExpense/:id' ,  expense.deleteExpense) // delete a expense
-
-router.post('/edit-expense/:id' , expense.editExpense)
-
-router.post('/get-expense' ,expense.getExpenses )
+const authenticate = require('../middleware/auth')
 
 
-//router.get('/download' , authenticate , expense.downloadExpenses)
-//router.get('/get-all-urls' , authenticate , expense.downloadUrls)
+router.get('/' ,authenticate, expense.getAll) //fetch all the expense 
+
+router.post('/add-expense' , authenticate,expense.addExpense) // add a new expense
+
+router.delete('/deleteExpense/:id' , authenticate, expense.deleteExpense) // delete a expense
+
+router.post('/edit-expense/:id' , authenticate,expense.editExpense)
+
+router.post('/get-expense' , authenticate ,expense.getExpenses )
 
 
 module.exports = router;
